b1ba260bc7b3fed4bd4774f42d3c718cad98169a
Maria Nesterova <82315940+ridebyhorse@users.noreply.github.com>
2024-09-29 18:57:00 +0300

    add base assets

---
+++ TicTacToe/Screens/Start/StartView.swift
@@ -0,0 +1,34 @@
+//
+//  StartView.swift
+//  TicTacToe
+//
+//  Created by Мария Нестерова on 29.09.2024.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Image(.mainIcon)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.horizontal, 48)
+            Text("TIC-TAC-TOE")
+                .font(.mainTitle)
+                .padding(30)
+            Spacer()
+            BasicButton(
+                styleType: .primary,
+                title: "Let's play",
+                tapHandler: { print("Let's") }
+            )
+        }
+        .padding(.horizontal, 21)
+    }
+}
+
+#Preview {
+    StartView()
+}
---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,22 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Мария Нестерова on 29.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [
+            .font : Font.navigationTitle
+        ]
+    }
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}

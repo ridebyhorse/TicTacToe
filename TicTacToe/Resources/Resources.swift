59599d50e2b59903a40c37002f313910a8563ff7
Maria Nesterova <82315940+ridebyhorse@users.noreply.github.com>
2024-09-30 19:26:57 +0300

    add result screen

--- TicTacToe/Resources/Resources.swift
+++ TicTacToe/Resources/Resources.swift
@@ -15,6 +15,11 @@ enum Resources {
         static let howToPlay = "How to Play"
         static let ticTacToe = "TIC-TAC-TOE"
         static let letsPlay = "Let's play"
+        static let winResult = " Win!"
+        static let loseResult = "You lose!"
+        static let drawResult = "Draw!"
+        static let playAgain = "Play again"
+        static let back = "Back"
     }
     
     enum Image {

1511692b8694b437291c84ec6129c9eecd99bce2
Ylyas Abdywahytow <ylyasabdywahytow@Ylyass-MacBook-Pro.local>
2024-10-05 11:48:58 -0400

    added LauchBar

--- TicTacToe/LaunchScreen/LaunchScreen.swift
+++ TicTacToe/LaunchScreen/LaunchScreen.swift
@@ -2,50 +2,68 @@ import SwiftUI
 
 struct LaunchScreen: View {
     @State private var isAnimating = false
+    @State private var textIndex = 0
+    @State private var isTextVisible = false
+    let textArray = Array("Tic-Tac-Toe")
     
     var body: some View {
-        VStack {
-            Spacer()
-            ZStack {
-                VStack{
-                    Image("tictac2")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .scaleEffect(isAnimating ? 2.5 : 3.7)
-                        .opacity(0.8)
-                }
+        ZStack {
+            Color.basicBackground.ignoresSafeArea(.all)
+            
+            VStack {
+                Spacer()
                 
-                HStack{
-                    Image("tictac")
+                HStack {
+                    Image("Launch1")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .scaleEffect(isAnimating ? 3 : 4)
-                        .padding(.top,350)
+                        .scaleEffect(isAnimating ? 0.8 : 1)
+                        .offset(y: isAnimating ? 0 : -300)
+                    
+                    Image("Launch2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .scaleEffect(isAnimating ? 0.8 : 1)
+                        .offset(y: isAnimating ? 0 : -300)
                 }
-                .padding(30)
+                .padding(.horizontal, 30)
+                
+                Spacer()
             }
-            .padding(.horizontal, 150)
-            .padding(.bottom, 80)
             .onAppear {
                 withAnimation(
-                    Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)
+                    .spring(response: 1, dampingFraction: 0.5).delay(0.2)
                 ) {
-                    isAnimating.toggle()
+                    isAnimating = true
                 }
             }
             
-            Spacer()
-            
-            
-            Text("Tic-Tac-Toe")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.basicBackground)
-                .padding(.bottom, 40)
+            VStack {
+                Spacer()
+                
+                HStack(spacing: 2) {
+                    ForEach(0..<textArray.count, id: \.self) { index in
+                        Text(String(textArray[index]))
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.basicBlack)
+                            .opacity(isTextVisible && textIndex >= index ? 1 : 0)
+                            .animation(.easeIn(duration: 0.1).delay(0.05 * Double(index)), value: isTextVisible)
+                    }
+                }
+                .onAppear {
+                    isTextVisible = true
+                    Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { timer in
+                        if textIndex < textArray.count {
+                            textIndex += 1
+                        } else {
+                            timer.invalidate()
+                        }
+                    }
+                }
+                .padding(.bottom, 60)
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.launch)
-        .ignoresSafeArea()
     }
 }
 
